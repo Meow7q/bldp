b79f7ca92111ecf7afb19f58ed9194319ce0fc52
Meow7 <17681888837@163.com>
2021-06-25 17:53:16 +0800

    bugfix: 1

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,10 +24,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
-        $schedule->command('command:test')->everyMinute();
-        $schedule->command('command:import-to-db')->everyMinute();
+         $schedule->command('command:test')
+                  ->everyMinute();
+         $schedule->command('command:import-to-db')
+                  ->everyMinute();
     }
 
     /**
